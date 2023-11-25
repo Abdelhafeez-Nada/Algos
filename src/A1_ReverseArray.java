8e25d0c180f4517aaa09117f609c6a3556719b89
Abdelhafeez <abdelhafeez0nada@gmail.com>
2023-11-25 21:21:26 +0200

    Reverse Array

---
+++ src/A1_ReverseArray.java
@@ -0,0 +1,33 @@
+public class A1_ReverseArray {
+
+    public static void main(String[] args) {
+
+        Util.printArr(loopRev(new int[] { 1, 2, 3, 4, 5 }));
+        Util.printArr(recRev(0, new int[] { 1, 2, 3, 4, 5 }));
+    }
+
+    // loop and two pointers
+
+    private static int[] loopRev(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (right > left) {
+            Util.swap(arr, left, right);
+            right--;
+            left++;
+        }
+
+        return arr;
+    }
+
+    // recursion two pointers
+
+    private static int[] recRev(int left, int[] arr) {
+
+        if (left >= arr.length - left - 1)
+            return arr;
+        Util.swap(arr, left, arr.length - left - 1);
+        return recRev(left + 1, arr);
+    }
+
+}
